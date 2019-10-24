66ce3ce2c6bac38793273ff1730a36748d651997
Isaac Ortega Arguedas <irsacfac@gmail.com>
2019-10-24 15:16:12 -0600

    Se creo el archivo json

--- src/otros/IConstants.java
+++ src/otros/IConstants.java
@@ -5,4 +5,5 @@ public interface IConstants {
 	public static int PANTALLA_ALTURA = 350;
 	public static int DEFAULT_BUTTON_ANCHURA = 80;
 	public static int DEFAULT_BUTTON_ALTURA = 40;
+	public static int TEXTLABEL_ANCHURA = 120;
 }
