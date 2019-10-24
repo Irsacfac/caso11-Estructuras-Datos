f6dc416ebc89cd7e53a31a1813844993c3500847
JorgeGuty <49891177+JorgeGuty@users.noreply.github.com>
2019-10-24 15:39:50 -0600

    JSONReader con singleton implementado

--- src/lector/JSONReader.java
+++ src/lector/JSONReader.java
@@ -1,5 +1,30 @@
 package lector;
 
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class JSONReader {
 
+
+    public static JSONReader getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    public JSONObject parseJson(String pFilePath){
+        JSONParser parser = new JSONParser();
+        try {
+            FileReader reader = new FileReader(pFilePath);
+            return (JSONObject) parser.parse(reader);
+        } catch (IOException | ParseException ioException) {
+            ioException.printStackTrace();
+        }
+        return null;
+    }
+
+    private static class SingletonHolder {
+        private static final JSONReader INSTANCE = new JSONReader();
+    }
 }
