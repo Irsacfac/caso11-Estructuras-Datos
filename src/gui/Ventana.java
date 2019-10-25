c9ef3b33f02359c4ff95e135eaa57da7e3eb9441
Isaac Ortega Arguedas <irsacfac@gmail.com>
2019-10-24 20:07:04 -0600

    Cambios

--- src/gui/Ventana.java
+++ src/gui/Ventana.java
@@ -1,37 +1,64 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.Frame;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import otros.IConstants;
 
 public class Ventana extends JFrame implements IConstants{
+	private JPanel buttonsPanel;
+	private JPanel textPanel;
+	private JTextField textbar;
+	private JButton botonPalabra;
+	private JButton botonRango;
+	private JButton botonURL;
 	
 	public Ventana() {
-		this("No Title");
+		this("No title");
 	}
-	
+
 	public Ventana(String pTitle) {
 		super(pTitle);
-		
 		this.setLayout(null);
 		this.setSize(PANTALLA_ANCHURA, PANTALLA_ALTURA);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setExtendedState(Frame.NORMAL);
-        //this.addMouseListener(this);
         
         initComponents();
         
         this.setVisible(true);
 	}
-
+	
 	private void initComponents() {
-		// TODO Auto-generated method stub
-		JPanel panel = new JPanel();
-		this.getContentPane().add(panel);
+		buttonsPanel = new JPanel();
+		buttonsPanel.setBounds(0, PANTALLA_ALTURA/2, PANTALLA_ANCHURA, PANTALLA_ALTURA/2);
+		buttonsPanel.setBackground(Color.CYAN);
+		this.getContentPane().add(buttonsPanel);
+		
+		textPanel = new JPanel();
+		textPanel.setBounds(0, 0, PANTALLA_ANCHURA, PANTALLA_ALTURA/2);
+		textPanel.setLayout(null);
+		textPanel.setBackground(Color.CYAN);
+		this.getContentPane().add(textPanel);
+		
+		textbar = new JTextField();
+		textbar.setBounds(X_TEXTLABEL, (int)PANTALLA_ALTURA/4, TEXTLABEL_ANCHURA, DEFAULT_BUTTON_ALTURA);
+		textPanel.add(textbar);
+		
+		botonPalabra = new JButton("Buscar Palabra");
+		buttonsPanel.add(botonPalabra);
+		
+		botonRango = new JButton("Buscar Rango");
+		buttonsPanel.add(botonRango);
+		
+		botonURL = new JButton("Buscar URL");
+		buttonsPanel.add(botonURL);
 	}
 }
--- src/otros/IConstants.java
+++ src/otros/IConstants.java
@@ -1,9 +1,10 @@
 package otros;
 
 public interface IConstants {
-	public static int PANTALLA_ANCHURA = 600;
-	public static int PANTALLA_ALTURA = 350;
-	public static int DEFAULT_BUTTON_ANCHURA = 80;
-	public static int DEFAULT_BUTTON_ALTURA = 40;
-	public static int TEXTLABEL_ANCHURA = 120;
+	public static int PANTALLA_ANCHURA = 500;
+	public static int PANTALLA_ALTURA = 250;
+	public static int DEFAULT_BUTTON_ANCHURA = 125;
+	public static int DEFAULT_BUTTON_ALTURA = 30;
+	public static int TEXTLABEL_ANCHURA = (PANTALLA_ANCHURA*80)/100;
+	public static int X_TEXTLABEL = TEXTLABEL_ANCHURA/8;
 }
